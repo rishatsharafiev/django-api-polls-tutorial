d52ee5de439cc0273c8ab7e052eb324a6c8cc923
Rishat Sharafiev <rishatsharafiev@ya.ru>
2020-07-03 14:09:32 +0300

    Add access control and permissions

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-from polls.apiviews import PollViewSet, ChoiceList, CreateVote
+from polls.apiviews import PollViewSet, ChoiceList, CreateVote, UserCreate, LoginView
 
 
 router = DefaultRouter()
@@ -10,6 +11,8 @@ router.register('polls', PollViewSet, basename='polls')
 urlpatterns = [
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+    path("users/", UserCreate.as_view(), name="user_create"),
+    path("login/", views.obtain_auth_token, name="login"),
 ]
 
 urlpatterns += router.urls
