ccc85f7e758754817539538b89ae1a96840c7f31
Rishat Sharafiev <rishatsharafiev@ya.ru>
2020-07-03 12:34:01 +0300

    Add ApiView classes

---
+++ polls/apiviews.py
@@ -0,0 +1,24 @@
+from rest_framework import generics
+from polls.models import Poll, Choice
+from polls.serializers import (
+    PollSerializer, ChoiceSerializer, VoteSerializer
+)
+
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
