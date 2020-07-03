3abd87d3e942e31ea800a79290ec5383fa8dbeda
Rishat Sharafiev <rishatsharafiev@ya.ru>
2020-07-03 12:12:37 +0300

    Create simple api

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from polls.models import Poll, Choice, Vote
+
+admin.site.register(Poll)
+admin.site.register(Choice)
+admin.site.register(Vote)
